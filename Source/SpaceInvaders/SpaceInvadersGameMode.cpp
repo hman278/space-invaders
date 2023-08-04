8e0ed589b2539c0082334d6839b88039d2c1d46e
hman278 <hyudyman@gmail.com>
2023-08-04 22:05:35 +0200

    base game done, enjoy

--- Source/SpaceInvaders/SpaceInvadersGameMode.cpp
+++ Source/SpaceInvaders/SpaceInvadersGameMode.cpp
@@ -7,20 +7,4 @@
 
 ASpaceInvadersGameMode::ASpaceInvadersGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = ASpaceInvadersPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if (PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
 }
